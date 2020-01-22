a17500e48c816d52b3137f1aabb54e638b0718cd
nazarkychma <kychmanazar@gmail.com>
2020-01-22 16:37:20 +0200

    Update queues route

--- app/routes/queues.py
+++ app/routes/queues.py
@@ -13,33 +13,32 @@ queues_service = JSONService("app/data/queues.json")
 @queues.route('/queues', methods=['POST'])
 @permission_required('create_queues')
 def create_queues():
-    if not request.json:
+    if not request.json['name']:
         abort(404)
 
     try:
         queues_json = queues_service.read()
-        values = queues_json.values()
-        if request.json['name'] in values:
+        if request.json['name'] in queues_json.values():
             abort(404)
         ids = list(queues_json.keys())
         queue_id = 0 if not ids else int(ids[-1]) + 1
         queue = {str(queue_id): request.json['name']}
         queues_json.update(queue)
         nodes_json = node_service.read()
+        response = {'id': queue_id,
+                    'name': request.json['name']}
         keys = list(nodes_json.keys())
         if keys:
-            data_to_send = {'id': queue_id,
-                            'name': request.json['name']}
             header = {'Authorization': create_manager_token()}
             for i in keys:
                 url = 'http://' + nodes_json[i]['ip'] + ':' + str(nodes_json[i]['port'])
                 ping = requests.get(url=url + '/ping')
                 if ping.text == 'pong':
-                    requests.post(url + '/queues', json=data_to_send, headers=header)
+                    requests.post(url + '/queues', json=response, headers=header)
         else:
             return jsonify(msg='Connect node first'), 404
         queues_service.write(queues_json)
-        return 'Queue created', 201
+        return jsonify(response), 201
     except Exception as e:
         print(e)
         return 'Something went wrong', 404
@@ -52,7 +51,7 @@ def delete_queue(queue_id):
         queues_json = queues_service.read()
         name = queues_json[str(queue_id)]
         if not name:
-            abort(404)
+            return "There is no queue with id {}".format(queue_id), 404
         nodes_json = node_service.read()
         keys = list(nodes_json.keys())
         if keys:
